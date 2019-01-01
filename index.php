b9a48d79d37b9defe4bcf4bf5cbdf8018cdcd65e
CDAracena <cdaracena@gmail.com>
2019-01-01 15:24:57 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">https://github.com/CDAracena/simplyPHP.git
+    <title></title>
+    <style>
+    .container {
+      max-width: 1200px;
+      margin: 0 auto;
+
+    };
+
+    .header {
+      margin: 100px;
+    };
+    </style>
+  </head>
+  <body>
+    <div class="container">
+    <header>
+      <nav>
+        <a href="home.php"> Home </a>
+        <a href="about.php">About </a>
+        <a href="projects.php"> Projects </a>
+        <a href="contacts.php"> Contacts </a>
+      </nav>
+
+    </header>
+    <section>
+      <h1> Home </h1>
+
+    </section>
+  </div>
+  </body>
+</html>
