4c666042d84a4df23bb278ca31df87cd67ad361a
CDAracena <cdaracena@gmail.com>
2019-01-01 17:16:24 -0500

    added basic css

--- index.php
+++ index.php
@@ -1,35 +1,26 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <meta charset="utf-8">https://github.com/CDAracena/simplyPHP.git
-    <title></title>
-    <style>
-    .container {
-      max-width: 1200px;
-      margin: 0 auto;
-
-    };
-
-    .header {
-      margin: 100px;
-    };
-    </style>
+    <meta charset="utf-8">
+    <title>Simple PHP</title>
+      <link rel="stylesheet" href="./styles.css" type="text/css">
   </head>
   <body>
     <div class="container">
     <header>
       <nav>
-        <a href="home.php"> Home </a>
-        <a href="about.php">About </a>
+        <a href="index.php"> Home </a>
+        <a href="about.php"> About </a>
         <a href="projects.php"> Projects </a>
-        <a href="contacts.php"> Contacts </a>
+        <a href="contact"> Contact </a>
       </nav>
 
     </header>
-    <section>
-      <h1> Home </h1>
 
-    </section>
-  </div>
+      <section>
+    <h1 class="home-title"> Home </h1>
+  </section>
+</div>
+
   </body>
 </html>
